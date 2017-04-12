af0aee99df6e47a9454f7821860a3470d2eaa802
shreki <smartherd@gmail.com>
2017-04-12 15:57:42 +0530

    Speech to text Functionality

--- app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
@@ -1,14 +1,51 @@
 package com.sriyanksiddhartha.speechtotext;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TextView txvResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		txvResult = (TextView) findViewById(R.id.txvResult);
+	}
 
+	public void getSpeechInput(View view) {
+
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+		if (intent.resolveActivity(getPackageManager()) != null) {
+			startActivityForResult(intent, 10);
+		} else {
+			Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+			case 10:
+				if (resultCode == RESULT_OK && data != null) {
+					ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+					txvResult.setText(result.get(0));
+				}
+				break;
+		}
 	}
 }
